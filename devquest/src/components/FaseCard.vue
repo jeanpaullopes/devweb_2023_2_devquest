b10c06f3238bb02f5a3bb31eb443b2e92bc4a7fe
jeanpaullopes <jean.lopes@ulife.com.br>
2023-11-17 20:10:03 -0300

    parte 1

---
+++ devquest/src/components/FaseCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div >
+    asdasd≤<br/>
+    {{ objFase.background }}
+    asdasd≤<br/>asdasd≤<br/>asdasd≤<br/>asdasd≤<br/>asdasd≤<br/>
+</div>
+
+  <q-card class="fasecard"
+    :style="getBackground()">
+    <q-card-section>
+      <h5>{{ objFase.titulo }}</h5>
+      {{ objFase.texto }}
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import axios from 'src/boot/axios';
+export default {
+  name: 'FaseCard',
+  props: {
+    fase: {
+      type: Object,
+      required: false
+    }
+  },
+  data() {
+    return {
+      objFase: Object
+    }
+  },
+  methods: {
+    getBackground() {
+      return 'background: url('+this.objFase.background+')'
+    }
+  },
+  computed: {
+  },
+  created() {
+    const axios = require('axios');
+    const consulta = axios.get('http://localhost:3000/fases/'+
+                                this.fase)
+    .then((response)=>{
+      this.objFase = response.data
+      console.log(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  },
+}
+</script>
+
+<style>
+  .fasecard {
+      width: 90vw;
+    }
+</style>
--- devquest/src/pages/IndexPage.vue
+++ devquest/src/pages/IndexPage.vue
@@ -1,17 +1,16 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <FaseCard fase="1"/>
+
   </q-page>
 </template>
 
 <script>
+import FaseCard from 'src/components/FaseCard.vue';
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+    name: 'IndexPage',
+    components: { FaseCard }
 })
 </script>
